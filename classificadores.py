23fee837ea10fe533334e993dfed851d05851639
Jean Carlo Corso <jeancarlocorso@hotmail.com>
2018-10-10 09:07:13 -0300

    metodo silhuetta

--- classificadores.py
+++ classificadores.py
@@ -1,5 +1,6 @@
 from sklearn.cluster import KMeans
 from sklearn_extensions.fuzzy_kmeans import KMedians, FuzzyKMeans
+from sklearn import metrics
 import numpy as np
 
 def PegaDados():
@@ -27,25 +28,41 @@ def k_means(dados):
     return kmeans
 
 def fuzzy_means(dados):
-    fuzzy_kmeans = FuzzyKMeans(k=2, m=2)
+    fuzzy_kmeans = FuzzyKMeans(k=2, m=1000)
     fuzzy_kmeans.fit(dados)
     
     return fuzzy_kmeans
 
+def comp_labels(kmeans, fuzzy, labels):
+    acuraciak = np.sum(labels == kmeans.labels_)/labels.shape[0]
+    acuraciaF = np.sum(labels == fuzzy.labels_)/labels.shape[0]
+
+    return acuraciak, acuraciaF
+
+def comp_silhuetta(kmeans, fuzzy, dados, metric='euclidean'):
+    metrica_sil_K = metrics.silhouette_score(dados, kmeans.labels_, metric)
+    metrica_sil_F = metrics.silhouette_score(dados, fuzzy.labels_, metric)
+
+    return metrica_sil_K, metrica_sil_F
+
 def main():
     dados, labels = PegaDados()
     kmeans = k_means(dados)
-    print(kmeans.labels_)
-    print()
-    print(labels)
-    print()
-    print("Acuracia: ",np.sum(labels == kmeans.labels_)/labels.shape[0])
     fuzzy = fuzzy_means(dados)
+    acuraciak, acuraciaF = comp_labels(kmeans, fuzzy, labels)
+    metrica_sil_K, metrica_sil_F = comp_silhuetta(kmeans, fuzzy, dados)
+    print("\nLabels gerados pelo K-means\n")
+    print(kmeans.labels_)
+    print("\nLabels gerados pelo Fuzzy-means\n")
     print(fuzzy.labels_)
-    print()
+    print("\nLabels originais do dataset\n")
     print(labels)
     print()
-    print("Acuracia: ",np.sum(labels == fuzzy.labels_)/labels.shape[0])
+    print("Metrica silhuetta k-means: ",metrica_sil_K)
+    print("Acuracia K-means: ",acuraciak)
+    print()
+    print("Metrica silhuetta Fuzzy-means: ",metrica_sil_F)
+    print("Acuracia Fuzzy-means: ", acuraciaF)
 
 main()
 
